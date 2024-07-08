6b15691670ef3ca25a384a45e2e8dbec8e03f1b4
skitheom <SahoKitahara@my.uopeople.edu>
2024-07-08 16:01:20 +0900

    ex00

--- go00/ex00/main.go
+++ go00/ex00/main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"ft"
 	"piscine"
 )
 
 func main() {
-	piscine.PrintAlphabet()
+	if err := piscine.PrintAlphabet(); err != nil {
+		ft.PrintRune('E')
+		ft.PrintRune('r')
+		ft.PrintRune('r')
+		ft.PrintRune('o')
+		ft.PrintRune('r')
+	}
 }
